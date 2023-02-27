37f82a4c8bf06e7844221a37e3825c378ada8f01
aboublef <kristopherab@gmail.com>
2023-02-27 01:43:46 +0000

    update app on action

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!\n"))
+		w.Write([]byte("Hello, Deployed application!\n"))
 	})
 
 	http.ListenAndServe(":"+port, nil)
